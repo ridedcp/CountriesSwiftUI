0604d16be89d69f7953c5154e4a003a80cc8947b
Daniel Cano <ridedcp@gmail.com>
2025-05-13 19:55:28 +0200

    add native search

--- CountriesSwiftUI/CountriesSwiftUI/Views/CountryListView.swift
+++ CountriesSwiftUI/CountriesSwiftUI/Views/CountryListView.swift
@@ -12,47 +12,40 @@ struct CountryListView: View {
 
     var body: some View {
         NavigationView {
-            VStack {
-                TextField("Search country or capital", text: $viewModel.searchText)
-                    .padding(10)
-                    .background(Color(.systemGray6))
-                    .cornerRadius(8)
-                    .padding([.horizontal, .top])
+            Group {
+                if viewModel.isLoading {
+                    ProgressView("Loading countries...")
+                        .padding()
+                } else if let error = viewModel.errorMessage {
+                    Text("Error: \(error)")
+                        .foregroundColor(.red)
+                        .padding()
+                } else {
+                    List(viewModel.countries) { country in
+                        NavigationLink(destination: CountryDetailView(country: country)) {
+                            HStack {
+                                AsyncImage(url: URL(string: country.flags.png)) { image in
+                                    image.resizable()
+                                } placeholder: {
+                                    Color.gray
+                                }
+                                .frame(width: 40, height: 25)
+                                .cornerRadius(4)
 
-                Group {
-                    if viewModel.isLoading {
-                        ProgressView("Loading countries...")
-                            .padding()
-                    } else if let error = viewModel.errorMessage {
-                        Text("Error: \(error)")
-                            .foregroundColor(.red)
-                            .padding()
-                    } else {
-                        List(viewModel.countries) { country in
-                            NavigationLink(destination: CountryDetailView(country: country)) {
-                                HStack {
-                                    AsyncImage(url: URL(string: country.flags.png)) { image in
-                                        image.resizable()
-                                    } placeholder: {
-                                        Color.gray
-                                    }
-                                    .frame(width: 40, height: 25)
-                                    .cornerRadius(4)
-
-                                    VStack(alignment: .leading) {
-                                        Text(country.name.common)
-                                            .font(.headline)
-                                        if let capital = country.capital?.first {
-                                            Text("Capital: \(capital)")
-                                                .font(.subheadline)
-                                                .foregroundColor(.secondary)
-                                        }
+                                VStack(alignment: .leading) {
+                                    Text(country.name.common)
+                                        .font(.headline)
+                                    if let capital = country.capital?.first {
+                                        Text("Capital: \(capital)")
+                                            .font(.subheadline)
+                                            .foregroundColor(.secondary)
                                     }
                                 }
                             }
                         }
-                        .listStyle(.plain)
                     }
+                    .listStyle(.plain)
+                    .searchable(text: $viewModel.searchText, prompt: "Search country or capital")
                 }
             }
             .navigationTitle("Countries in the world")
